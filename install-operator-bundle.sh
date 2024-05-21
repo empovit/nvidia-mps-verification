04b81181cdef776fd992f4eb98c35a1dec1582bb
Vitaliy Emporopulo <vemporop@redhat.com>
2024-05-21 16:28:58 +0300

    Remove privileged label from namespace for now

--- install-operator-bundle.sh
+++ install-operator-bundle.sh
@@ -8,10 +8,7 @@ BUNDLE=${BUNDLE:-"registry.gitlab.com/nvidia/kubernetes/gpu-operator/staging/gpu
 
 oc create namespace $GPU_OPERATOR_NAMESPACE
 
-operator-sdk version | grep '\-ocp'
-if [[ $? == 0 ]]; then
-    echo "WARNING: Running an OCP build of operator-sdk, will apply 'pod-security.kubernetes.io/enforce=privileged' label to the namespace"
-    oc label namespace $GPU_OPERATOR_NAMESPACE pod-security.kubernetes.io/enforce=privileged  --overwrite
-fi
+# echo "WARNING: Will apply 'pod-security.kubernetes.io/enforce=privileged' label to the namespace"
+# oc label namespace $GPU_OPERATOR_NAMESPACE pod-security.kubernetes.io/enforce=privileged  --overwrite
 
 operator-sdk run bundle --timeout=20m -n $GPU_OPERATOR_NAMESPACE --install-mode OwnNamespace ${BUNDLE}
