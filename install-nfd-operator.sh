28595f85d06a4063f884365e8d83891a16db225f
Vitaliy Emporopulo <vemporop@redhat.com>
2024-05-21 12:55:38 +0300

    Create NFD instance from CVS default

--- install-nfd-operator.sh
+++ install-nfd-operator.sh
@@ -4,6 +4,12 @@ set -e
 set -o pipefail
 
 oc apply -f nfd-operator.yaml
+
 CSV_NAME=$(oc get csv -n openshift-nfd -o name)
+
 oc wait --for=jsonpath='{.status.phase}'=Succeeded --timeout=360s -n openshift-nfd ${CSV_NAME}
-oc apply -f nfd-instance.yaml
+
+NFD_INSTANCE=$(oc get ${CSV_NAME} -n openshift-nfd -o jsonpath='{.metadata.annotations.alm-examples}' | jq -r 'map(select(.kind == "NodeFeatureDiscovery")) | .[0]')
+oc apply -f - <<EOF
+$NFD_INSTANCE
+EOF
